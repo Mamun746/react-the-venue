dd891d5c7d3b2bba31777d6dd72bd9714ab906e4
Mamun <abdullahalmamun914@gmail.com>
2019-01-09 10:42:50 +0600

    Updates

---
+++ precache-manifest.5d0b96e644a12ea850b6b2c90904a777.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/react-the-venue/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/react-the-venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/react-the-venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "c85905123248e6584a7debd6b3c400ff",
+    "url": "/react-the-venue/static/media/slide_four.c8590512.jpg"
+  },
+  {
+    "revision": "0fb2d2b47b06344f8ba8",
+    "url": "/react-the-venue/static/js/runtime~main.0fb2d2b4.js"
+  },
+  {
+    "revision": "eef4fcb7da329a9d0c61",
+    "url": "/react-the-venue/static/js/main.eef4fcb7.chunk.js"
+  },
+  {
+    "revision": "a1a22faa47c597d3bcb5",
+    "url": "/react-the-venue/static/js/1.a1a22faa.chunk.js"
+  },
+  {
+    "revision": "eef4fcb7da329a9d0c61",
+    "url": "/react-the-venue/static/css/main.0ccbbc76.chunk.css"
+  },
+  {
+    "revision": "195e133d5f222482bdc76bc9a3c8f75c",
+    "url": "/react-the-venue/index.html"
+  }
+];
